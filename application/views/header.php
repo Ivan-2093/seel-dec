36fe075fafb7cef0e2a25794f5a849c25b847dda
Sergio <programador3@codiesel.co>
2023-07-11 14:57:47 -0500

    UPDATE SEELDEC
    CHECK

--- application/views/header.php
+++ application/views/header.php
@@ -1,49 +1,74 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-?>
-
-<!DOCTYPE html>
-<html lang="es">
+<?php $img_user = $this->session->userdata('img_user') ?>
+<!doctype html>
+<html class="no-js" lang="es">
 
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>CODIESEL SA - <?= $page ?></title>
 	<meta name="description" content="">
-	<meta name="author" content="Sergio Ivan Galvis">
 	<meta name="keywords" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link rel="shortcut icon" href="<?= base_url() ?>plantilla/img/icons/logo-seeldec.jpeg" />
+	<!--<link rel="icon" href="favicon.ico" type="image/x-icon" />-->
 
-	<link rel="canonical" href="https://demo-basic.adminkit.io/" />
+	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
 
-	<title>SEELDEC</title>
-
-	<link href="<?= base_url() ?>plantilla/css/app.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
-	<!-- Jquery -->
-	<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-	<!-- Select2  -->
+	<link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/fontawesome-free/css/all.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/icon-kit/dist/css/iconkit.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/ionicons/dist/css/ionicons.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/perfect-scrollbar/css/perfect-scrollbar.css">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/jvectormap/jquery-jvectormap.css">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/weather-icons/css/weather-icons.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/c3/c3.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/owl.carousel/dist/assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/owl.carousel/dist/assets/owl.theme.default.min.css">
+	<!-- <link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.css"> -->
+	<!-- <link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/mohithg-switchery/dist/switchery.min.css"> -->
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/dist/css/theme.min.css">
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css"><!-- datetimepicker -->
+	<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/dist/css/sliderEncuesta.css">
+	<script src="<?= base_url() ?>plantilla/src/js/vendor/modernizr-2.8.3.min.js"></script>
 	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-	<!-- SweetAlert2 -->
-	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-	<!-- Datatable.net -->
-	<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
-	<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
-	<?php $this->load->helper('estilos_helper'); ?>
-
+	<link rel="stylesheet" href="<?= base_url() ?>plantilla/plugins/Wizard-JS-main/styles/css/main.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>calendar/lib/main.css">
+	<script src="<?= base_url() ?>calendar/lib/main.min.js"></script>
+	<script src="<?= base_url() ?>calendar/lib/es.js"></script>
+	<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.38.0/css/tempusdominus-bootstrap-4.min.css" crossorigin="anonymous" /> -->
 </head>
 
-<body>
+<body data-editor="ClassicEditor">
+	<div class="loader" id="cargando"></div>
+	<!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
 	<div class="wrapper">
-		<?php $this->load->view('menu'); ?>
+		<header class="header-top" header-theme="light">
+			<div class="container-fluid">
+				<div class="d-flex justify-content-between">
+					<div class="top-menu d-flex align-items-center">
+						<button type="button" class="btn-icon mobile-nav-toggle d-lg-none"><span></span></button>
+						<div class="header-search">
+							<div class="input-group">
+								<span class="input-group-addon search-close"><i class="ik ik-x"></i></span>
+								<input type="text" class="form-control">
+								<span class="input-group-addon search-btn"><i class="ik ik-search"></i></span>
+							</div>
+						</div>
+						<button type="button" id="navbar-fullscreen" class="nav-link"><i class="ik ik-maximize"></i></button>
+					</div>
+					<?php $this->load->view('navbar'); ?>
+				</div>
+			</div>
+		</header>
 
-		<div class="main">
-
-			<?php $this->load->view('nav'); ?>
-
-			<main class="content">
-				<div class="container-fluid p-0">
-					<div class="loader" id="cargando"></div>
+		<div class="page-wrap">
+			<?php $this->load->view('menu'); ?>
+			<div class="main-content">
--- application/views/menu.php
+++ application/views/menu.php
@@ -1,98 +1,151 @@
-<nav id="sidebar" class="sidebar js-sidebar">
-	<div class="sidebar-content js-simplebar">
-		<a class="sidebar-brand" href="#">
-			<span class="align-middle">SEEL-DEC</span>
-		</a>
+<div class="app-sidebar colored">
+    <div class="sidebar-header">
+        <a class="header-brand" href="../index.html">
+            <div class="logo-img">
+                <img src="../src/img/brand-white.svg" class="header-brand-img" alt="lavalite">
+            </div>
+            <span class="text">ThemeKit</span>
+        </a>
+        <button type="button" class="nav-toggle"><i data-toggle="expanded" class="ik ik-toggle-right toggle-icon"></i></button>
+        <button id="sidebarClose" class="nav-close"><i class="ik ik-x"></i></button>
+    </div>
 
-		<ul class="sidebar-nav">
-			<!-- <li class="sidebar-header">
-						Pagess
-					</li> -->
+    <div class="sidebar-content">
+        <div class="nav-container">
+            <nav id="main-menu-navigation" class="navigation-main">
+                <div class="nav-lavel">Navigation</div>
+                <div class="nav-item">
+                    <a href="../index.html"><i class="ik ik-bar-chart-2"></i><span>Dashboard</span></a>
+                </div>
+                <div class="nav-item">
+                    <a href="navbar.html"><i class="ik ik-menu"></i><span>Navigation</span> <span class="badge badge-success">New</span></a>
+                </div>
+                <div class="nav-item has-sub">
+                    <a href="javascript:void(0)"><i class="ik ik-layers"></i><span>Widgets</span> <span class="badge badge-danger">150+</span></a>
+                    <div class="submenu-content">
+                        <a href="widgets.html" class="menu-item">Basic</a>
+                        <a href="widget-statistic.html" class="menu-item">Statistic</a>
+                        <a href="widget-data.html" class="menu-item">Data</a>
+                        <a href="widget-chart.html" class="menu-item">Chart Widget</a>
+                    </div>
+                </div>
+                <div class="nav-lavel">UI Element</div>
+                <div class="nav-item has-sub">
+                    <a href="#"><i class="ik ik-box"></i><span>Basic</span></a>
+                    <div class="submenu-content">
+                        <a href="ui/alerts.html" class="menu-item">Alerts</a>
+                        <a href="ui/badges.html" class="menu-item">Badges</a>
+                        <a href="ui/buttons.html" class="menu-item">Buttons</a>
+                        <a href="ui/navigation.html" class="menu-item">Navigation</a>
+                    </div>
+                </div>
+                <div class="nav-item has-sub">
+                    <a href="#"><i class="ik ik-gitlab"></i><span>Advance</span> <span class="badge badge-success">New</span></a>
+                    <div class="submenu-content">
+                        <a href="ui/modals.html" class="menu-item">Modals</a>
+                        <a href="ui/notifications.html" class="menu-item">Notifications</a>
+                        <a href="ui/carousel.html" class="menu-item">Slider</a>
+                        <a href="ui/range-slider.html" class="menu-item">Range Slider</a>
+                        <a href="ui/rating.html" class="menu-item">Rating</a>
+                    </div>
+                </div>
+                <div class="nav-item has-sub">
+                    <a href="#"><i class="ik ik-package"></i><span>Extra</span></a>
+                    <div class="submenu-content">
+                        <a href="ui/session-timeout.html" class="menu-item">Session Timeout</a>
+                    </div>
+                </div>
+                <div class="nav-item">
+                    <a href="ui/icons.html"><i class="ik ik-command"></i><span>Icons</span></a>
+                </div>
+                <div class="nav-lavel">Forms</div>
+                <div class="nav-item has-sub">
+                    <a href="#"><i class="ik ik-edit"></i><span>Forms</span></a>
+                    <div class="submenu-content">
+                        <a href="form-components.html" class="menu-item">Components</a>
+                        <a href="form-addon.html" class="menu-item">Add-On</a>
+                        <a href="form-advance.html" class="menu-item">Advance</a>
+                    </div>
+                </div>
+                <div class="nav-item">
+                    <a href="form-picker.html"><i class="ik ik-terminal"></i><span>Form Picker</span> <span class="badge badge-success">New</span></a>
+                </div>
 
-			<li class="sidebar-item">
-				<a class="sidebar-link" href="<?php echo base_url() ?>UsuariosController">
-					<i class="align-middle" data-feather="user"></i> <span class="align-middle">Usuarios</span>
-				</a>
-			</li>
+                <div class="nav-lavel">Tables</div>
+                <div class="nav-item">
+                    <a href="table-bootstrap.html"><i class="ik ik-credit-card"></i><span>Bootstrap Table</span></a>
+                </div>
+                <div class="nav-item">
+                    <a href="table-datatable.html"><i class="ik ik-inbox"></i><span>Data Table</span></a>
+                </div>
 
-			<li class="sidebar-item">
-				<a class="sidebar-link" href="<?php echo base_url() ?>TercerosController">
-					<i class="align-middle" data-feather="users"></i> <span class="align-middle">Terceros</span>
-				</a>
-			</li>
-			<li class="sidebar-item">
-				<a class="sidebar-link" href="<?php echo base_url() ?>EmpleadosController">
-					<i class="align-middle" data-feather="users"></i> <span class="align-middle">Empleados</span>
-				</a>
-			</li>
+                <div class="nav-lavel">Charts</div>
+                <div class="nav-item has-sub">
+                    <a href="#"><i class="ik ik-pie-chart"></i><span>Charts</span> <span class="badge badge-success">New</span></a>
+                    <div class="submenu-content">
+                        <a href="charts-chartist.html" class="menu-item">Chartist</a>
+                        <a href="charts-flot.html" class="menu-item">Flot</a>
+                        <a href="charts-knob.html" class="menu-item">Knob</a>
+                        <a href="charts-amcharts.html" class="menu-item">Amcharts</a>
+                    </div>
+                </div>
 
-			<!-- <li class="sidebar-item">
-						<a class="sidebar-link" href="pages-sign-in.html">
-							<i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Sign In</span>
-						</a>
-					</li>
+                <div class="nav-lavel">Apps</div>
+                <div class="nav-item">
+                    <a href="calendar.html"><i class="ik ik-calendar"></i><span>Calendar</span></a>
+                </div>
+                <div class="nav-item">
+                    <a href="taskboard.html"><i class="ik ik-server"></i><span>Taskboard</span></a>
+                </div>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages-sign-up.html">
-							<i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Sign Up</span>
-						</a>
-					</li>
+                <div class="nav-lavel">Pages</div>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages-blank.html">
-							<i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
-						</a>
-					</li>
-
-					<li class="sidebar-header">
-						Tools & Components
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-buttons.html">
-							<i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span>
-						</a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-forms.html">
-							<i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
-						</a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-cards.html">
-							<i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
-						</a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-typography.html">
-							<i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
-						</a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="icons-feather.html">
-							<i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
-						</a>
-					</li>
-
-					<li class="sidebar-header">
-						Plugins & Addons
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="charts-chartjs.html">
-							<i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
-						</a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="maps-google.html">
-							<i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
-						</a>
-					</li> -->
-		</ul>
-	</div>
-</nav>
+                <div class="nav-item has-sub">
+                    <a href="#"><i class="ik ik-lock"></i><span>Authentication</span></a>
+                    <div class="submenu-content">
+                        <a href="login.html" class="menu-item">Login</a>
+                        <a href="register.html" class="menu-item">Register</a>
+                        <a href="forgot-password.html" class="menu-item">Forgot Password</a>
+                    </div>
+                </div>
+                <div class="nav-item has-sub">
+                    <a href="#"><i class="ik ik-file-text"></i><span>Other</span></a>
+                    <div class="submenu-content">
+                        <a href="profile.html" class="menu-item">Profile</a>
+                        <a href="invoice.html" class="menu-item">Invoice</a>
+                    </div>
+                </div>
+                <div class="nav-item active">
+                    <a href="layouts.html"><i class="ik ik-layout"></i><span>Layouts</span><span class="badge badge-success">New</span></a>
+                </div>
+                <div class="nav-lavel">Other</div>
+                <div class="nav-item has-sub">
+                    <a href="javascript:void(0)"><i class="ik ik-list"></i><span>Menu Levels</span></a>
+                    <div class="submenu-content">
+                        <a href="javascript:void(0)" class="menu-item">Menu Level 2.1</a>
+                        <div class="nav-item has-sub">
+                            <a href="javascript:void(0)" class="menu-item">Menu Level 2.2</a>
+                            <div class="submenu-content">
+                                <a href="javascript:void(0)" class="menu-item">Menu Level 3.1</a>
+                            </div>
+                        </div>
+                        <a href="javascript:void(0)" class="menu-item">Menu Level 2.3</a>
+                    </div>
+                </div>
+                <div class="nav-item">
+                    <a href="javascript:void(0)" class="disabled"><i class="ik ik-slash"></i><span>Disabled Menu</span></a>
+                </div>
+                <div class="nav-item">
+                    <a href="javascript:void(0)"><i class="ik ik-award"></i><span>Sample Page</span></a>
+                </div>
+                <div class="nav-lavel">Support</div>
+                <div class="nav-item">
+                    <a href="javascript:void(0)"><i class="ik ik-monitor"></i><span>Documentation</span></a>
+                </div>
+                <div class="nav-item">
+                    <a href="javascript:void(0)"><i class="ik ik-help-circle"></i><span>Submit Issue</span></a>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
