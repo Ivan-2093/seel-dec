08db2cc2515b3acef8f56372ee400df54d10b045
Sergio <programador3@codiesel.co>
2023-07-18 17:52:58 -0500

    UPDATE EMAIL
    A

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -92,4 +92,86 @@ class LoginController extends CI_Controller
         $this->session->sess_destroy();
         header("Location: " . base_url());
     }
+
+    public function restPassword()
+    {
+        $username = $this->input->POST('username');
+        if ($username != "" && $username != NULL) {
+            $data_where = array(
+                'usuario' => $username
+            );
+            $data_user = $this->UsuariosModel->getUserByNameUser($data_where);
+
+            if ($data_user->num_rows() > 0) {
+                $data_whereEmp = array('e.id' => $data_user->row(0)->empleado_id);
+                $data_empleado = $this->EmpleadosModel->getEmpleadosByIdEmpleado($data_whereEmp);
+                print_r($data_empleado->row(0)->correo);
+            } else {
+            }
+        } else {
+        }
+    }
+
+    public function sendEmail()
+    {
+        $this->load->library('phpmailer_lib');
+        $correo = $this->phpmailer_lib->load();
+
+        // SMTP configuration
+        $correo->IsSMTP();
+        $correo->SMTPDebug = 1;
+        $correo->SMTPAuth = true;
+        $correo->SMTPSecure = 'tls';
+        $correo->Host = "mail.aftersalesassistance.com";
+        $correo->Port = 587;
+        $correo->IsHTML(true);
+        $correo->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+        $correo->Username = "developer@aftersalesassistance.com";
+        $correo->Password = "kA0&!7cQ(ws(";
+        $correo->SetFrom("developer@aftersalesassistance.com", "CODIESEL S.A");
+        $correo->addAddress("programador3@codiesel.co");
+        $correo->Subject = "Solicitud ausentismo ";
+
+        $mensaje = '<!DOCTYPE html>
+				<html>
+				<head>
+				<meta charset="utf-8">
+				<title></title>
+				<style type="text/css">
+									#boton_aceptar{
+				background-color: #199319;
+				color: white;
+				padding: 15px 25px;
+				text-decoration: none;
+				}
+									#boton_rechasar{
+					background-color: red;
+					color: white;
+					padding: 15px 25px;
+					text-decoration: none;
+				}
+				</style>
+				</head>
+				<body>
+				<div style="padding: 20px;">
+                HDPTA
+				</div>
+				<div style="padding: 20px;display: inline-block;">
+				</div>
+				</body>
+				</html>';
+        $correo->MsgHTML($mensaje);
+
+        if (!$correo->Send()) {
+            echo "err";
+        } else {
+            echo 'ok';
+        }
+    }
 }
