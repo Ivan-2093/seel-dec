3d3a85949552f3f57f96969fdfa0f85a2ed74344
Sergio Galvis <sergioivangalvisesteban@gmail.com>
2023-07-31 22:37:06 -0500

    asdf
    asdf

--- application/controllers/PermisosController.php
+++ application/controllers/PermisosController.php
@@ -54,68 +54,73 @@ class PermisosController extends CI_Controller
     {
 
         $html_permisos_perfil = '';
-
+        $perfil_id = $this->input->post('perfil_id');
         $array_menus = $this->MenusModel->getMenus();
-        foreach ($array_menus->result() as $key) {
-            $data_where = array(
-                'menu_id' => $key->id_menu,
-                'perfil_id' => $this->perfil
-            );
-
-            $val_menu = $this->MenusModel->getMenuPerfilByIdMenu($data_where);
-            $checked = "";
-            if ($val_menu->num_rows() > 0) {
-                $checked = "checked";
-            }
-            $html_permisos_perfil .= '
-			<section class="">
-			<div class="card">
-				<div class="card-header">
-					<div class="row">
-						<div class="col-auto">
-							<strong class="card-title">' . $key->menu . '</strong>
-						</div>
-						<div class="col-auto">
-							<input class="toggle-one" id="' . $key->menu . '" ' . $checked . ' type="checkbox" data-width="30" data-height="20"
-							data-onstyle="success" onchange="check_menu(\'' . $key->menu . '\',' . $key->id_menu . ',' . $this->perfil . ');"  data-toggle="toggle" data-size="xs">							
-						</div>
-					</div>
-		
-				</div>
-				<div class="card-body">';
-            $data_where_sub = array(
-                'menu_id' => $key->id_menu,
-            );
-            $submenus = $this->MenusModel->getSubmenusWhere($data_where_sub);
-            foreach ($submenus->result() as $sm) {
-
-                $data_submenu_perfil = array(
-                    'perfil_id' => $this->perfil,
-                    'submenu_id' => $sm->id_submenu,
+        if ($array_menus->num_rows() > 0) {
+            foreach ($array_menus->result() as $key) {
+                $data_where = array(
+                    'menu_id' => $key->id_menu,
+                    'perfil_id' => $perfil_id
                 );
 
-                $val_submenu = $this->MenusModel->getMenuPerfilByIdSubmenu($data_submenu_perfil);
-                $checked2 = "";
-                if ($val_submenu->num_rows() > 0) {
-                    $checked2 = "checked";
+                $val_menu = $this->MenusModel->getMenuPerfilByIdMenu($data_where);
+                $checked = "";
+                if ($val_menu->num_rows() > 0) {
+                    $checked = "checked";
                 }
-
-
                 $html_permisos_perfil .= '
-							<input class="toggle-one" id="' . $sm->submenu . '" ' . $checked2 . ' type="checkbox" data-width="20" data-height="20"
-							data-toggle="toggle" onchange="check_submenu(\'' . $sm->submenu . '\',' . $sm->id_submenu . ',' . $this->perfil . ');" data-size="xs">
-							<label>' . $sm->submenu . '</label>';
-            }
-            $html_permisos_perfil .= '
-				</div>
-			</div>
-		    </section>
-			';
-        }
-        $data_response = array(
-            'html_permisos_perfil' => $html_permisos_perfil
-        );
-        echo json_encode($data_response);
+                <section class="">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-auto">
+                                <strong class="card-title">' . $key->menu . '</strong>
+                            </div>
+                            <div class="col-auto">
+                                <input class="toggle-one" id="' . $key->menu . '" ' . $checked . ' type="checkbox" data-width="30" data-height="20"
+                                data-onstyle="success" onchange="check_menu(\'' . $key->menu . '\',' . $key->id_menu . ',' . $perfil_id . ');"  data-toggle="toggle" data-size="xs">							
+                            </div>
+                        </div>
+            
+                    </div>
+                    <div class="card-body">';
+                $data_where_sub = array(
+                    'menu_id' => $key->id_menu,
+                );
+                $submenus = $this->MenusModel->getSubmenusWhere($data_where_sub);
+                if ($submenus->num_rows() > 0) {
+                    foreach ($submenus->result() as $sm) {
 
+                        $data_submenu_perfil = array(
+                            'perfil_id' => $this->perfil,
+                            'submenu_id' => $sm->id_submenu,
+                        );
+
+                        $val_submenu = $this->MenusModel->getMenuPerfilByIdSubmenu($data_submenu_perfil);
+                        $checked2 = "";
+                        if ($val_submenu->num_rows() > 0) {
+                            $checked2 = "checked";
+                        }
+
+
+                        $html_permisos_perfil .= '
+                                <input class="toggle-one" id="' . $sm->submenu . '" ' . $checked2 . ' type="checkbox" data-width="20" data-height="20"
+                                data-toggle="toggle" onchange="check_submenu(\'' . $sm->submenu . '\',' . $sm->id_submenu . ',' . $this->perfil . ');" data-size="xs">
+                                <label>' . $sm->submenu . '</label>';
+                    }
+                }
+                $html_permisos_perfil .= '
+                    </div>
+                </div>
+                </section>
+                ';
+            }
+            $data_response = array(
+                'html_permisos_perfil' => $html_permisos_perfil
+            );
+        }
+
+
+        echo json_encode($data_response);
     }
 }
