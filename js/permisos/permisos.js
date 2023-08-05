0dcf8e7fd9107e9c2c359ea4ebcc7ed1aa5060f2
Sergio <programador3@codiesel.co>
2023-08-05 11:47:21 -0500

    UPDATE
    update permisos

--- js/permisos/permisos.js
+++ js/permisos/permisos.js
@@ -28,6 +28,10 @@ $(".js-select2-perfiles").on("change", function (e) {
 			})
 			.then(function (json) {
 				content_permisos.innerHTML = json["html_permisos_perfil"];
+				$('.toggle-one').bootstrapToggle({
+                    on: "<i class='fas fa-check-circle'></i>",
+                    off: "<i class='fas fa-ban'></i>"
+                });
 				hiddenLoading(cargando);
 			})
 			.catch(function (error) {
@@ -46,3 +50,68 @@ $(".js-select2-perfiles").on("change", function (e) {
 			});
 	}
 });
+
+function check_menu(menu,menu_id,perfil_id,input){
+	showLoading(cargando);
+	const options = (input.checked === true ? 1 : 0); 
+	const formulario_permisos = new FormData();
+	formulario_permisos.append('menu_id',menu_id);
+	formulario_permisos.append('perfil_id',perfil_id);
+	formulario_permisos.append('options',options);
+	fetch(`${base_url}PermisosController/update_permisos_perfil`, {
+		headers: {
+			"Content-type": "application/json",
+		},
+		mode: "no-cors",
+		method: "POST",
+		body: formulario_permisos,
+	})
+		.then(function (response) {
+			// Transforma la respuesta. En este caso lo convierte a JSON
+			return response.json();
+		})
+		.then(function (json) {
+			switch (true) {
+				case json['response'] === 'success':
+					Swal.fire({
+						title: "EXITO",
+						html: `${json['mensaje']} Se cerrará en <strong></strong> segundos.<br/><br/>`,
+						icon: json['response'],
+						confirmButtonText: "Ok",
+						allowOutsideClick: false,
+						showCloseButton: true,
+						timer: 5000,
+						didOpen: () => {
+						timerInterval = setInterval(() => {
+							Swal.getHtmlContainer().querySelector('strong')
+							  .textContent = (Swal.getTimerLeft() / 1000)
+								.toFixed(0)
+						  }, 100)
+						},
+						willClose: () => {
+						  clearInterval(timerInterval)
+						}
+					});
+					hiddenLoading(cargando);
+					break;
+			
+				default:
+					break;
+			}
+			
+		})	
+		.catch(function (error) {
+			Swal.fire({
+				title: "ERROR",
+				html: `Ha ocurrido un error:( <strong>${error}</strong> ), contacte con el departamento de sistemas o intentenuavamente.`,
+				icon: "error",
+				confirmButtonText: "Ok",
+				allowOutsideClick: false,
+				showCloseButton: true,
+				willClose: () => {
+					/* location.reload(); */
+				},
+			});
+			hiddenLoading(cargando);
+		});
+}
